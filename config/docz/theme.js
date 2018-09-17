0f7c886b6500cac1acdb2044bc90cd26ff8f26d5
sungwon.cha <mygenie3@naver.com>
2018-09-17 21:46:31 +0900

    fix

--- config/docz/theme.js
+++ config/docz/theme.js
@@ -1,8 +1,38 @@
 export default {
   colors: {
-    primary: 'tomato',
+    // primary: 'tomato',
+    // primary: '#6d4c41',
+    // primary: '#7cb342',
+    primary: '#1e88e5',
+    blue: '#1e88e5',
   },
   styles: {
+    h1: {
+      fontSize: 32,
+      fontWeight: 700,
+      letterSpacing: 0,
+      '&:before': {
+        height: '3px !important',
+        // backgroundColor: 'yellow',
+      },
+
+    },
+    h2: {
+      fontSize: 24,
+      fontWeight: 700,
+      letterSpacing: 0,
+    },
+    // wrapper: {
+    //   background: 'yellow !important',
+    //   '&:before': {
+    //     height: '3px !important',
+    //     backgroundColor: 'yellow',
+    //   },
+    // },
+    paragraph: {
+      fontSize: 16,
+      lineHeight: 1.5,
+    },
     // h1: {
     //   fontSize: 100,
     // },
