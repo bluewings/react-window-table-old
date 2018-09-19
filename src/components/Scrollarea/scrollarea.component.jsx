96489ebde0ea4ee7f22cfdea7a79c1a8e0d40de9
sungwon.cha <mygenie3@naver.com>
2018-09-19 10:53:04 +0900

    add throttled scroll

--- src/components/Scrollarea/scrollarea.component.jsx
+++ src/components/Scrollarea/scrollarea.component.jsx
@@ -1,193 +1,137 @@
 /* eslint-disable react/prop-types, no-underscore-dangle, react/no-unused-state */
-/* eslint-disable */
+/* eslint-disabl */
 import React, { PureComponent } from 'react';
 import { compose, defaultProps } from 'recompose';
-// import sampleImg from './sample.png';
+import { Map } from 'immutable';
+import memoizeOne from 'memoize-one';
 
-// jsx, styles
+// jsx
 import template from './scrollarea.component.pug';
+
+const THROTTLED_SCROLL = 100;
 
 class Scrollarea extends PureComponent {
   constructor(props) {
     super(props);
-    // this.state = { fieldValue: {}, scrollTop: 0, scrollLeft: 0 };
-    this.state = { scrollTop: 0, scrollLeft: 0 };
     this.bumperRef = React.createRef();
     this.shimRef = React.createRef();
     this.scrollWrapRef = React.createRef();
     this.scrollContentRef = React.createRef();
+    this.state = { scrollTop: 0, scrollLeft: 0 };
+    this.scrollInfo = Map({ scrollTop: 0, scrollLeft: 0 });
   }
 
   componentDidMount() {
     if (this.bumperRef.current && this.scrollWrapRef.current) {
       this.scrollWrapRef.current.style.width = this.bumperRef.current.style.width;
     }
-    // window.addEventListener('wheel', (event) => {
-    //   // // // console.log(event);
-    //   // // console.log('wheel', event);
-    //   event.preventDefault();
-    //   // event.preventDefault();
-    //   // event.stopPropagation();
-    //   return false;
-    // });
-    // window.addEventListener('mousewheel', (event) => {
-    //   // // console.log('mousewheel', event);
-    //   // event.preventDefault();
-    //   // event.stopPropagation();
-    //   // return false;
-    // });
   }
 
   componentDidUpdate() {
-    // console.log('www');
     if (this.bumperRef.current && this.scrollWrapRef.current) {
-      // // // console.log(this.bumperRef.current.style.width);
-      this.scrollWrapRef.current.style.width = `${
-        this.bumperRef.current.getBoundingClientRect().width
-      }px`;
+      const bumperWidth = this.bumperRef.current.getBoundingClientRect().width;
+      this.scrollWrapRef.current.style.width = `${bumperWidth}px`;
     }
   }
 
-  handleValueChange = fieldValue => {
-    this.setState({ fieldValue });
-  };
+  handleMousewheel = (event) => {
+    const scrollContentRect = this.scrollContentRef.current.getBoundingClientRect();
+    const scrollWrapRect = this.scrollWrapRef.current.getBoundingClientRect();
 
-  // handleTouchstart = (event) => {
-  //   // // console.log('%c touchstart', 'background:yellow');
-  //   // // console.log(event);
-  // }
+    const contentWidth = this.props.contentWidth || scrollContentRect.width;
+    const contentHeight = this.props.contentHeight || scrollContentRect.height;
 
-  handleMousewheel = event => {
-    // // // console.log(this.scrollContent)
-
-    // var wheelDistance = function(evt){
-    //   if (!evt) evt = event;
-    //   var w=evt.wheelDelta, d=evt.detail;
-    //   if (d){
-    //     if (w) return w/d/40*d>0?1:-1; // Opera
-    //     else return -d/3;              // Firefox;         TODO: do not /3 for OS X
-    //   } else return w/120;             // IE/Safari/Chrome TODO: /3 for Chrome OS X
-    // };
-    // // // console.log(event.deltaX, event.deltaY);
-    const contentHeight =
-      this.props.contentHeight ||
-      this.scrollContentRef.current.getBoundingClientRect().height;
-    const scrollHeight =
-      contentHeight - this.scrollWrapRef.current.getBoundingClientRect().height;
-
-    const contentWidth =
-      this.props.contentWidth ||
-      this.scrollContentRef.current.getBoundingClientRect().width;
-
-    const scrollWidth = Math.max(
-      contentWidth - this.scrollWrapRef.current.getBoundingClientRect().width,
-      0,
-    );
+    const scrollWidth = Math.max(contentWidth - scrollWrapRect.width, 0);
+    const scrollHeight = contentHeight - scrollWrapRect.height;
 
     let { deltaX, deltaY } = event;
 
-    // deltaX *= 1.2;
-    // deltaY *= 1.2;
+    const absDeltaX = Math.abs(deltaX);
+    const absDeltaY = Math.abs(deltaY);
 
-    const _deltaX = Math.abs(deltaX);
-    const _deltaY = Math.abs(deltaY);
-    if (_deltaX > _deltaY * 1.5) {
+    if (absDeltaX > absDeltaY * 1.5) {
       deltaY = 0;
-    } else if (_deltaY > _deltaX * 1.5) {
+    } else if (absDeltaY > absDeltaX * 1.5) {
       deltaX = 0;
     }
 
-    // if (Math.abs(deltaX > 0 ?))
-    // if (Math.ab)
-
     let { scrollLeft, scrollTop } = this.props;
+
     scrollTop += deltaY;
     if (scrollTop < 0) {
       scrollTop = 0;
-    }
-    // // // console.log(scrollTop, scrollHeight);
-    if (scrollTop > scrollHeight) {
+    } else if (scrollTop > scrollHeight) {
       scrollTop = scrollHeight;
     }
 
     scrollLeft += deltaX;
-
-    // console.log(scrollLeft, scrollWidth);
     if (scrollLeft < 0) {
       scrollLeft = 0;
-    }
-    // // // console.log(scrollTop, scrollHeight);
-    if (scrollLeft > scrollWidth) {
+    } else if (scrollLeft > scrollWidth) {
       scrollLeft = scrollWidth;
     }
-    // // // console.log(scrollLeft, scrollTop, scrollWidth);
 
-    // const _scrollLeft = Math.abs(event.deltaX);
-    // const _scrollTop = Math.abs(event.deltaY);
-    // // // console.log(_scrollTop / _scrollLeft)
-    // // console.log(scrollLeft, scrollTop);
+    const scrollInfo = this.scrollInfo.set('scrollTop', scrollTop).set('scrollLeft', scrollLeft);
 
-    if (
-      scrollTop !== this.state.scrollTop ||
-      scrollLeft !== this.state.scrollLeft
-    ) {
-      // // // console.log('>>> moving...');
-      // this.state.scrollLeft = scrollLeft;
-      // this.state.scrollTop = scrollTop;
+    if (this.scrollInfo !== scrollInfo) {
       event.preventDefault();
       event.stopPropagation();
-
       if (typeof this.props.onScroll === 'function') {
-        // requestAnimationFrame(() => {
-        // // console.log(scrollTop * -1);
-        // console.log(scrollLeft, scrollTop);
         this.props.onScroll({ scrollLeft, scrollTop });
-        // });
       }
-      this.setState(prevState => ({
-        ...prevState,
-        scrollTop,
-        scrollLeft,
-      }));
+      this.scrollInfo = scrollInfo;
+      this.handleThrottledScroll(scrollInfo);
     }
-    // else if (_scrollTop / _scrollLeft < 0.5) {
-    //   // 가로 이동이다!!!
-    //   // // console.log('>>> 가로 이동임....');
-    //   // event.preventDefault();
-    //   // event.stopPropagation();
-    //   // return false;
-    // }
-    // return undefined;
-    // event.preventDefault();
-    // event.stopPropagation();
   };
 
+  handleThrottledScroll = (scrollInfo) => {
+    clearTimeout(this._clearScrollId);
+    this.throttledScrollInfo = scrollInfo;
+    if (!this._scrollId) {
+      this._scrollId = setInterval(this.throttledScroll, THROTTLED_SCROLL);
+      this._scrollCount = 0;
+      this._scrollStart = new Date();
+      this.throttledScroll();
+    }
+    this._clearScrollId = setTimeout(() => {
+      clearInterval(this._scrollId);
+      this.throttledScroll();
+      delete this._scrollId;
+      // show scroll stats
+      const stats = {
+        elapsed: new Date() - this._scrollStart,
+        throttled: THROTTLED_SCROLL,
+        events: this._scrollCount,
+      };
+      console.table(stats);
+    }, THROTTLED_SCROLL + 50);
+  };
+
+  throttledScroll = () => {
+    if (this.throttledScrollInfo) {
+      delete this.throttledScrollInfo;
+      this._scrollCount += 1;
+    }
+  };
+
+  scrollWrapStyle = memoizeOne((width, height) => ({ width, height }));
+
   render() {
-    const { width, height } = this.props;
-    const { fieldValue, scrollLeft, scrollTop } = this.state;
-
-    const scrollWrapStyle = {
-      width,
-      height,
-    };
-
     return template.call(this, {
       // variables
-      unused_fieldValue: fieldValue,
-      unused_scrollLeft: scrollLeft,
-      unused_scrollTop: scrollTop,
-      scrollWrapStyle,
+      scrollWrapStyle: this.scrollWrapStyle(
+        this.props.width,
+        this.props.height,
+      ),
     });
   }
 }
 
-const enhance = compose(
-  defaultProps({
-    width: 500,
-    height: 400,
-    contentWidth: null,
-    contentHeight: null,
-  }),
-);
+const enhance = compose(defaultProps({
+  width: 500,
+  height: 400,
+  contentWidth: null,
+  contentHeight: null,
+}));
 
 export default enhance(Scrollarea);
